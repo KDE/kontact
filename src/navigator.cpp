f3373d7e0a5b8b2f190ef5cbad50bd63b9089af7
Daniel Molkentin <molkentin@kde.org>
2002-04-09 22:35:29 +0000

    - some cleanups and refactoring in the bar - beautified the sidebar
    short term todos: tidy up indention, add proper license header
    
    svn path=/trunk/kdepim/kaplan/src/; revision=148307

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -1,63 +1,100 @@
 #include <qsignal.h>
-
+#include <qpainter.h>
+#include <qdrawutil.h>
 
 #include <kglobal.h>
 #include <kiconloader.h>
 
-
 #include "navigator.h"
 
 
-class Entry
-{
-public:
+EntryItem::EntryItem( QListBox *parent, const QPixmap& icon, const QString& name,
+                      QObject* receiver, const QCString& slot )
+  : QListBoxPixmap(KIconLoader::unknown() ) {
+  m_slot = slot;
+  m_receiver = receiver;
+  m_parent = parent;
+  m_Pixmap = new QPixmap(icon);
+  setCustomHighlighting( true );
+  setText( name );
+}
 
-  QObject *receiver;
-  QCString slot;
-  int index;
-  
-};
+EntryItem::~EntryItem() {
+}
+
+void EntryItem::connectToReceiver(QObject* sender) {
+  QSignal sig(sender);
+  sig.connect(m_receiver, m_slot);
+  sig.activate();
+}
+
+int EntryItem::width( const QListBox *listbox) const {
+  return listbox->viewport()->width();
+}
+
+int EntryItem::height( const QListBox *listbox) const {
+  int min = 0;
+  min = listbox->fontMetrics().lineSpacing() + pixmap()->height() + 6;
+  return min;
+}
+
+void EntryItem::paint(QPainter *p)
+{
+  QListBox *box = listBox();
+  int w = width( box );
+  static const int margin = 3;
+  int y = margin;
+  const QPixmap *pm = pixmap();
+
+  if ( !pm->isNull() ) {
+    int x = (w - pm->width()) / 2;
+    x = QMAX( x, margin );
+    p->drawPixmap( x, y, *pm );
+  }
+
+  if ( !text().isEmpty() ) {
+    QFontMetrics fm = p->fontMetrics();
+    y += pm->height() + fm.height() - fm.descent();
+    int x = (w - fm.width( text() )) / 2;
+    x = QMAX( x, margin );
+    p->drawText( x, y, text() );
+  }
+  // draw sunken
+  if ( isCurrent() || isSelected() ) {
+    qDrawShadePanel( p, 1, 0, w -2, height(box),
+                     box->colorGroup(), true, 1, 0L );
+  }
+}
 
 
 Navigator::Navigator(QWidget *parent, const char *name)
-  : KListBox(parent, name), m_index(0)
+  : KListBox(parent, name)
 {
-  m_entries.setAutoDelete(true);
 
-  connect(this, SIGNAL(executed(QListBoxItem *)), this, SLOT(slotExecuted(QListBoxItem *)));
+  setSelectionMode( KListBox::Single );
+  QPalette pal = palette();
+  QColor gray = pal.color(QPalette::Normal, QColorGroup::Mid );
+  pal.setColor( QPalette::Normal, QColorGroup::Base, gray );
+  pal.setColor( QPalette::Inactive, QColorGroup::Base, gray );
+
+  setPalette( pal );
+  viewport()->setBackgroundMode( PaletteMid);
+
+
+  connect(this, SIGNAL(executed(QListBoxItem *)), this, SLOT(slotExecuted(EntryItem *)));
 }
 
 
 void Navigator::addEntry(QString text, QString icon, QObject *receiver, const char *slot)
 {
   QPixmap pixmap = KGlobal::iconLoader()->loadIcon(icon, KIcon::Desktop, 48);
-  insertItem(pixmap, text, m_index);
-
-  Entry *e = new Entry;
-  e->receiver = receiver;
-  e->slot = slot;
-  e->index = m_index;
-  m_entries.append(e);
-
-  m_index++;
+  insertItem(new EntryItem(this, pixmap, text, receiver, slot));
 }
 
 
-void Navigator::slotExecuted(QListBoxItem *item)
+void Navigator::slotExecuted(EntryItem *item)
 {
-  int i = index(item);
-  if (i<0)
-    return;
-
-  QListIterator<Entry> it(m_entries);
-  for ( ; it.current(); ++it)
-    if (it.current()->index == i)
-      {
-        QSignal sig(this);
-        sig.connect(it.current()->receiver, it.current()->slot);
-        sig.activate();
-        return;
-      }
+  item->connectToReceiver(this);
 }
 
 
--- src/navigator.h
+++ src/navigator.h
@@ -1,12 +1,53 @@
 #ifndef __NAVIGATOR_H__
 #define __NAVIGATOR_H__
 
-
 #include <klistbox.h>
 #include <qlist.h>
 
+class QPixmap;
+class QListBox;
 
-class Entry;
+class EntryItem : public QListBoxPixmap
+{
+  public:
+      EntryItem(QListBox *, const QPixmap&, const QString&, QObject*, const QCString&);
+      ~EntryItem();
+      /**
+       * sets the icon for the item.
+       * @param icon the icon to set
+       * @param group the icongroup
+       */
+      void setIcon( const QString& icon, KIcon::Group group = KIcon::Panel );
+
+      /* Does the virtal connection
+       */
+      void connectToReceiver( QObject * );
+
+      /**
+       * returns the width of this item.
+       */
+      virtual int width( const QListBox * ) const;
+      /**
+       * returns the height of this item.
+       */
+      virtual int height( const QListBox * ) const;
+
+      /**
+       * returns the pixmap.
+       */
+      virtual const QPixmap * pixmap() const {
+        return m_Pixmap;
+      }
+
+  protected:
+      virtual void paint( QPainter *p);
+
+  private:
+      QPixmap* m_Pixmap;
+      QObject* m_receiver;
+      QCString m_slot;
+      QListBox* m_parent;
+};
 
 
 class Navigator : public KListBox
@@ -22,16 +63,16 @@ public:
 
 private slots:
 
-  void slotExecuted(QListBoxItem *item);
+  void slotExecuted(EntryItem *item);
 
 
 private:
-
+/*
   QList<Entry> m_entries;
 
   int m_index;
+*/
 
 };
-
 
 #endif
