2dc0bffc405c19b590eb8a7629a14465986c1561
Laurent Montel <montel@kde.org>
2004-01-02 16:50:23 +0000

    Fix missing I18N_NOOP
    svn path=/trunk/kdepim/; revision=276074

--- plugins/summary/summaryview_plugin.cpp
+++ plugins/summary/summaryview_plugin.cpp
@@ -54,7 +54,7 @@ const KAboutData *SummaryView::aboutData()
                                  "0.1",
                                  I18N_NOOP("Kontact Summary View"),
                                  KAboutData::License_LGPL,
-                                 "(c) 2003 The Kontact developers" );
+                                 I18N_NOOP("(c) 2003 The Kontact developers" ) );
     mAboutData->addAuthor( "Sven Lueppken", "", "sven@kde.org" );
     mAboutData->addAuthor( "Cornelius Schumacher", "", "schumacher@kde.org" );
     mAboutData->addAuthor( "Tobias Koenig", "", "tokoe@kde.org" );
