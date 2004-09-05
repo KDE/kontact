bd84ffb4b1a48750bfdacee7785c73ee8b3dfd36
Ingo Klcker <kloecker@kde.org>
2004-09-05 16:06:40 +0000

    Backport: Make the New Message action from KMail's Kontact plugin respect the folder identity. This made the creation of a new hidden DCOP call necessary.
    svn path=/branches/KDE_3_3_BRANCH/kdepim/; revision=344181

--- plugins/kmail/kmail_plugin.cpp
+++ plugins/kmail/kmail_plugin.cpp
@@ -88,7 +88,7 @@ void KMailPlugin::openComposer( const KURL& attach )
     if ( attach.isValid() )
       mStub->openComposer( "", "", "", "", "", false, KURL(), attach );
     else
-      mStub->openComposer( "", "", "", "", "", false );
+      mStub->newMessage();
   }
 }
 
