f276b024ebdcb6046121053e8f9a366f20c9d960
David Faure <faure@kde.org>
2004-11-30 21:38:25 +0000

    Don't call hide() on the current widget, it leads to the main kontact area not repainting itself at all, which looks very weird. This came from the fact that the todo and calendar plugins share the same part, so partLoaded() is called twice with the same part. https://intevation.de/roundup/kolab/issue555
    svn path=/trunk/kdepim/; revision=367472

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -450,8 +450,11 @@ void MainWindow::addPlugin( Kontact::Plugin *plugin )
 
 void MainWindow::partLoaded( Kontact::Plugin * /*plugin*/, KPIM::Part *part )
 {
-  if ( part->widget() )
-    mStack->addWidget( part->widget() );
+  // See if we have this part already (e.g. due to two plugins sharing it)
+  if ( mStack->id( part->widget() ) != -1 )
+    return;
+
+  mStack->addWidget( part->widget() );
 
   mPartManager->addPart( part, false );
   // Workaround for KParts misbehavior: addPart calls show!
