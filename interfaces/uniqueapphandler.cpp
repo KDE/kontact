efed6853f11b1b86e9974c853c7d6b4faf641e60
David Faure <faure@kde.org>
2005-05-26 19:45:42 +0000

    oops, this file wasn't branched yet... didn't intend to commit into 3.3-branch
    svn path=/branches/KDE/3.3/kdepim/; revision=418502

--- interfaces/uniqueapphandler.cpp
+++ interfaces/uniqueapphandler.cpp
@@ -103,12 +103,6 @@ bool UniqueAppHandler::process(const QCString &fun, const QByteArray &data, QCSt
 
        QDataStream _replyStream( replyData, IO_WriteOnly );
        _replyStream << newInstance( );
-    } else if ( fun == "load()" ) {
-      replyType = "bool";
-      (void)mPlugin->part(); // load the part without bringing it to front
-
-      QDataStream _replyStream( replyData, IO_WriteOnly );
-      _replyStream << true;
     } else {
         return DCOPObject::process( fun, data, replyType, replyData );
     }
@@ -126,7 +120,6 @@ QCStringList UniqueAppHandler::functions()
 {
     QCStringList funcs = DCOPObject::functions();
     funcs << "int newInstance()";
-    funcs << "bool load()";
     return funcs;
 }
 
