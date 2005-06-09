6f2858658c3381c181c0271e983f97a503242627
David Faure <faure@kde.org>
2005-06-09 17:19:42 +0000

    So rc1 was in fact rc2 (hmm), and now we're putting rc3 out after the recent bugfixes.
    svn path=/branches/kdepim/proko2/kdepim/; revision=423783

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.0 - proko2 rc1";
+static const char version[] = "1.0 - proko2 rc3";
 
 class KontactApp : public KUniqueApplication {
   public:
