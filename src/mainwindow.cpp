04aa58786f8eefbb7f2476acb5d340bf201570e2
Daniel Molkentin <molkentin@kde.org>
2005-06-09 12:01:51 +0000

    after tobias changes this is no longer needed, in the best case dangerous, so I'm got rid of it
    svn path=/trunk/KDE/kdepim/kontact/src/; revision=423696

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -153,9 +153,6 @@ void MainWindow::initObject()
     plugActionList( "navigator_actionlist", mSidePane->actions() );
   }
 
-  // flush paint events
-  kapp->processEvents();
-
   KSettings::Dispatcher::self()->registerInstance( instance(), this,
                                                    SLOT( updateConfig() ) );
 
