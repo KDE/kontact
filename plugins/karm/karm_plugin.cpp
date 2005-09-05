27c828387650a25d80f235f5b0754d5e92fb2733
Allen Winter <winter@kde.org>
2005-09-05 17:55:43 +0000

    forward port
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=457372

--- plugins/karm/karm_plugin.cpp
+++ plugins/karm/karm_plugin.cpp
@@ -42,7 +42,7 @@ KarmPlugin::KarmPlugin( Kontact::Core *core, const char *, const QStringList& )
   setInstance( KarmPluginFactory::instance() );
   (void)dcopClient();
   insertNewAction( new KAction( i18n( "New Task" ), "karm",
-                   Qt::CTRL+Qt::SHIFT+Qt::Key_T, this, SLOT( newTask() ), actionCollection(),
+                   Qt::CTRL+Qt::SHIFT+Qt::Key_W, this, SLOT( newTask() ), actionCollection(),
                    "new_task" ) );
 }
 
