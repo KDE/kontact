b37249a042305c99fe045925724bd5ef57374cbe
Allen Winter <winter@kde.org>
2006-03-15 17:27:58 +0000

    Nicer.  plus, this way, we don't have to worry about stripping out funky characters that may be in the URL title.
    svn path=/branches/KDE/3.5/kdepim/; revision=518914

--- plugins/newsticker/summarywidget.cpp
+++ plugins/newsticker/summarywidget.cpp
@@ -306,7 +306,7 @@ bool SummaryWidget::eventFilter( QObject *obj, QEvent* e )
   if ( obj->inherits( "KURLLabel" ) ) {
     KURLLabel* label = static_cast<KURLLabel*>( obj );
     if ( e->type() == QEvent::Enter )
-      emit message( i18n( "Open URL %1" ).arg( label->text() ) );
+      emit message( label->url() );
     if ( e->type() == QEvent::Leave )
       emit message( QString::null );
   }
