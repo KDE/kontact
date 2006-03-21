7e20de9074b1b16cbbabe75a01c94e41941a8e53
Allen Winter <winter@kde.org>
2006-03-21 23:19:46 +0000

    couldn't hurt.
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=521229

--- plugins/newsticker/summarywidget.cpp
+++ plugins/newsticker/summarywidget.cpp
@@ -144,8 +144,9 @@ void SummaryWidget::initDocuments()
     connectDCOPSignal( "rssservice", feedRef.obj(), "documentUpdated(DCOPRef)",
                        "documentUpdated(DCOPRef)", false );
 
-    qApp->processEvents( QEventLoop::ExcludeUserInput |
-                         QEventLoop::ExcludeSocketNotifiers );
+    if ( qApp )
+      qApp->processEvents( QEventLoop::ExcludeUserInput |
+                           QEventLoop::ExcludeSocketNotifiers );
   }
 
   updateDocuments();
@@ -171,8 +172,9 @@ void SummaryWidget::documentUpdated( DCOPRef feedRef )
     DCOPRef artRef = feedRef.call( "article(int)", i );
     QString title, url;
 
-    qApp->processEvents( QEventLoop::ExcludeUserInput |
-                         QEventLoop::ExcludeSocketNotifiers );
+    if ( qApp )
+      qApp->processEvents( QEventLoop::ExcludeUserInput |
+                           QEventLoop::ExcludeSocketNotifiers );
 
     artRef.call( "title()" ).get( title );
     artRef.call( "link()" ).get( url );
