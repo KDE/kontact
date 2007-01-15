9b2413620a0485c34458f22fc2f1a1edb27f2eab
Stephan Binner <binner@kde.org>
2007-01-15 09:27:47 +0000

    increase version
    svn path=/branches/KDE/3.5/kdepim/; revision=623701

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.3";
+static const char version[] = "1.2.4";
 
 class KontactApp : public KUniqueApplication {
   public:
