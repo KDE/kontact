8303783ec3a81c124fd9260bb262c79db496a0a3
Volker Krause <vkrause@kde.org>
2007-04-10 15:41:42 +0000

    update version numbers
    svn path=/branches/kdepim/enterprise/kdepim/; revision=652276

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.4 (enterprise 0.20070326.646712)";
+static const char version[] = "1.2.4 (enterprise 0.20070410.652270)";
 
 class KontactApp : public KUniqueApplication {
   public:
