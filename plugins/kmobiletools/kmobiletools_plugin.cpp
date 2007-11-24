9cf2e6597cf6e3ed4b351293eeb3a6a7c6f98ce2
Allen Winter <winter@kde.org>
2007-11-24 15:50:16 +0000

    minor formatting SVN_SILENT:
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=741007

--- plugins/kmobiletools/kmobiletools_plugin.cpp
+++ plugins/kmobiletools/kmobiletools_plugin.cpp
@@ -1,48 +1,47 @@
 /***************************************************************************
-   Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
-   Copyright (C) 2007 Marco Gulino <marco@kmobiletools.org>
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (C) 2007 Marco Gulino <marco@kmobiletools.org>
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the
-   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
  ***************************************************************************/
 
 #include "kmobiletools_plugin.h"
+#include <core.h>
+#include <maininterface.h>
 
-#include <kgenericfactory.h>
 #include <kparts/componentfactory.h>
 #include <kaboutdata.h>
-#include <kactioncollection.h>
-
-#include <core.h>
-#include <kapplication.h>
 #include <kaction.h>
-#include <maininterface.h>
+#include <kapplication.h>
+#include <kactioncollection.h>
+#include <kgenericfactory.h>
 
 typedef KGenericFactory<KMobileToolsPlugin, Kontact::Core> KMobileToolsPluginFactory;
 K_EXPORT_COMPONENT_FACTORY( kontact_kmobiletools,
                             KMobileToolsPluginFactory( "kontact_kmobiletools" ) )
 
-KMobileToolsPlugin::KMobileToolsPlugin( Kontact::Core *core, const QStringList& )
-    : Kontact::Plugin( core, core, "KMobileTools" ), partLoaded(false)
+KMobileToolsPlugin::KMobileToolsPlugin( Kontact::Core *core, const QStringList & )
+  : Kontact::Plugin( core, core, "KMobileTools" ), partLoaded( false )
 {
   setComponentData( KMobileToolsPluginFactory::componentData() );
-  KAction *newaction=new KAction(i18n( "New SMS..." ), this );
-  actionCollection()->addAction("newsms", newaction);
-  newaction->setShortcut(QKeySequence(Qt::CTRL+Qt::SHIFT+Qt::Key_S));
-  connect(newaction, SIGNAL(triggered(bool)),SLOT( slotNewSMS() ));
-  insertNewAction(newaction);
-  setExecutableName("kmobiletools_bin");
+  KAction *newaction = new KAction( i18n( "New SMS..." ), this );
+  actionCollection()->addAction( "newsms", newaction );
+  newaction->setShortcut( QKeySequence( Qt::CTRL+Qt::SHIFT + Qt::Key_S ) );
+  connect( newaction, SIGNAL(triggered(bool)),SLOT(slotNewSMS()) );
+  insertNewAction( newaction );
+  setExecutableName( "kmobiletools_bin" );
 }
 
 KMobileToolsPlugin::~KMobileToolsPlugin()
@@ -58,21 +57,22 @@ bool KMobileToolsPlugin::isRunningStandalone()
   return false;
 }
 
-KParts::ReadOnlyPart* KMobileToolsPlugin::createPart()
+KParts::ReadOnlyPart *KMobileToolsPlugin::createPart()
 {
-  KParts::ReadOnlyPart* m_part=loadPart();
+  KParts::ReadOnlyPart *m_part = loadPart();
   partLoaded=(bool)m_part;
 #ifdef __GNUC__
 #warning "look at if interface is ok";
 #endif
-  m_interface = new OrgKdeKmobiletoolsMainInterface("org.kde.kmobiletools", "/KMobitools", QDBusConnection::sessionBus() );
+  m_interface = new OrgKdeKmobiletoolsMainInterface( "org.kde.kmobiletools",
+                                                     "/KMobitools", QDBusConnection::sessionBus() );
   return m_part;
 }
 
 void KMobileToolsPlugin::slotNewSMS()
 {
   (void) part(); // ensure part is loaded
-  Q_ASSERT(m_interface);
+  Q_ASSERT( m_interface );
   m_interface->newSMS();
 }
 
--- plugins/kmobiletools/kmobiletools_plugin.h
+++ plugins/kmobiletools/kmobiletools_plugin.h
@@ -1,34 +1,32 @@
 /***************************************************************************
-   Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
-   Copyright (C) 2007 Marco Gulino <marco@kmobiletools.org>
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (C) 2007 Marco Gulino <marco@kmobiletools.org>
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the
-   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
  ***************************************************************************/
-
 
 #ifndef KMOBILETOOLS_PLUGIN_H
 #define KMOBILETOOLS_PLUGIN_H
 
-#include <klocale.h>
 #include <kparts/part.h>
+#include <klocale.h>
 
 #include <plugin.h>
 #include <uniqueapphandler.h>
 
 class OrgKdeKmobiletoolsMainInterface;
-
 
 class KMobileToolsPlugin : public Kontact::Plugin
 {
