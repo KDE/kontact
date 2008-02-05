90daa9c116e0cbbc8600566a86c27d126a90f65c
David Faure <faure@kde.org>
2008-02-05 09:17:15 +0000

    keine sync
    svn path=/trunk/KDE/kdepim/kontact/src/; revision=771097

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -69,7 +69,6 @@ void AboutDialog::saveSize()
 {
   KConfigGroup group(KGlobal::config(), "AboutDialog");
   saveDialogSize(group);
-  group.sync();
 }
 
 void AboutDialog::addAboutPlugin( Kontact::Plugin *plugin )
