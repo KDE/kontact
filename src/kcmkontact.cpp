cca32752455b5b6edf2fd6e6b76cce77054c54fd
Allen Winter <winter@kde.org>
2008-03-30 14:49:45 +0000

    use a KComboBox instead of a QComboBox, for that KDE look-and-feel fix a i18n string. re-arrange includes style
    svn path=/trunk/KDE/kdepim/kontact/src/; revision=791851

--- src/kcmkontact.cpp
+++ src/kcmkontact.cpp
@@ -1,53 +1,51 @@
 /*
-    This file is part of KDE Kontact.
+  This file is part of KDE Kontact.
 
-    Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #include "kcmkontact.h"
 #include "prefs.h"
-#include <kcomponentdata.h>
 
+#include <kdemacros.h>
+#include <kcomponentdata.h>
 #include <kaboutdata.h>
 #include <kdebug.h>
 #include <k3listview.h>
 #include <klocale.h>
 #include <kservicetypetrader.h>
+#include <kcombobox.h>
 
 #include <q3buttongroup.h>
 #include <QCheckBox>
-#include <QComboBox>
 #include <QLabel>
 #include <QLayout>
-//Added by qt3to4:
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QBoxLayout>
 
-#include <kdemacros.h>
-
 extern "C"
 {
   KDE_EXPORT KCModule *create_kontactconfig( QWidget *parent, const char * ) {
-	KComponentData inst("kcmkontact" );
+    KComponentData inst( "kcmkontact" );
     return new KcmKontact( inst, parent );
   }
 }
@@ -78,39 +76,40 @@ KcmKontact::KcmKontact( const KComponentData &inst, QWidget *parent )
   topLayout->addItem( pluginStartupLayout );
   topLayout->addStretch();
 
-  KPrefsWidBool *forceStartupPlugin = addWidBool( Kontact::Prefs::self()->forceStartupPluginItem(), this );
+  KPrefsWidBool *forceStartupPlugin =
+    addWidBool( Kontact::Prefs::self()->forceStartupPluginItem(), this );
   pluginStartupLayout->addWidget( forceStartupPlugin->checkBox() );
 
-  PluginSelection *selection = new PluginSelection( Kontact::Prefs::self()->forcedStartupPluginItem(), this );
+  PluginSelection *selection =
+    new PluginSelection( Kontact::Prefs::self()->forcedStartupPluginItem(), this );
   addWid( selection );
 
   pluginStartupLayout->addWidget( selection->comboBox() );
   selection->comboBox()->setEnabled( false );
 
-  connect( forceStartupPlugin->checkBox(), SIGNAL( toggled( bool ) ),
-           selection->comboBox(), SLOT( setEnabled( bool ) ) );
+  connect( forceStartupPlugin->checkBox(), SIGNAL(toggled(bool)),
+           selection->comboBox(), SLOT(setEnabled(bool)) );
   load();
 }
 
-const KAboutData* KcmKontact::aboutData() const
+const KAboutData *KcmKontact::aboutData() const
 {
   KAboutData *about = new KAboutData( I18N_NOOP( "kontactconfig" ), 0,
                                       ki18n( "KDE Kontact" ),
                                       0, KLocalizedString(), KAboutData::License_GPL,
                                       ki18n( "(c), 2003 Cornelius Schumacher" ) );
 
-  about->addAuthor( ki18n("Cornelius Schumacher"), KLocalizedString(), "schumacher@kde.org" );
-  about->addAuthor( ki18n("Tobias Koenig"), KLocalizedString(), "tokoe@kde.org" );
+  about->addAuthor( ki18n( "Cornelius Schumacher" ), KLocalizedString(), "schumacher@kde.org" );
+  about->addAuthor( ki18n( "Tobias Koenig" ), KLocalizedString(), "tokoe@kde.org" );
 
   return about;
 }
 
-
 PluginSelection::PluginSelection( KConfigSkeleton::ItemString *item, QWidget *parent )
 {
   mItem = item;
-  mPluginCombo = new QComboBox( parent );
-  connect( mPluginCombo, SIGNAL( currentIndexChanged( int ) ), SIGNAL( changed() ) );
+  mPluginCombo = new KComboBox( parent );
+  connect( mPluginCombo, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
 }
 
 PluginSelection::~PluginSelection()
@@ -129,13 +128,15 @@ void PluginSelection::readConfig()
     KService::Ptr service = *it;
     // skip summary only plugins
     QVariant var = service->property( "X-KDE-KontactPluginHasPart" );
-    if ( var.isValid() && var.toBool() == false )
+    if ( var.isValid() && var.toBool() == false ) {
       continue;
+    }
     mPluginCombo->addItem( service->name() );
     mPluginList.append( service );
 
-    if ( service->property("X-KDE-PluginInfo-Name").toString() == mItem->value() )
+    if ( service->property( "X-KDE-PluginInfo-Name" ).toString() == mItem->value() ) {
       activeComponent = mPluginList.count() - 1;
+    }
   }
 
   mPluginCombo->setCurrentIndex( activeComponent );
@@ -144,7 +145,7 @@ void PluginSelection::readConfig()
 void PluginSelection::writeConfig()
 {
   KService::Ptr ptr =  mPluginList.at( mPluginCombo->currentIndex() );
-  mItem->setValue( ptr->property("X-KDE-PluginInfo-Name").toString() );
+  mItem->setValue( ptr->property( "X-KDE-PluginInfo-Name" ).toString() );
 }
 
 QList<QWidget *> PluginSelection::widgets() const
--- src/kcmkontact.h
+++ src/kcmkontact.h
@@ -1,25 +1,25 @@
 /*
-    This file is part of KDE Kontact.
+  This file is part of KDE Kontact.
 
-    Copyright (c) 2003 Cornelius Scumacher <schumacher@kde.org>
+  Copyright (c) 2003 Cornelius Scumacher <schumacher@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #ifndef KCMKONTACT_H
@@ -32,7 +32,7 @@
 
 #include <QList>
 
-class QComboBox;
+class KComboBox;
 class KAboutData;
 
 using namespace KPIM;
@@ -44,7 +44,7 @@ class KcmKontact : public KPrefsModule
   public:
     explicit KcmKontact( const KComponentData &inst, QWidget *parent = 0 );
 
-    virtual const KAboutData* aboutData() const;
+    virtual const KAboutData *aboutData() const;
 };
 
 class PluginSelection : public KPrefsWid
@@ -59,10 +59,10 @@ class PluginSelection : public KPrefsWid
     void writeConfig();
 
     QList<QWidget *> widgets() const;
-    QComboBox *comboBox() const { return mPluginCombo; }
+    KComboBox *comboBox() const { return mPluginCombo; }
 
   private:
-    QComboBox *mPluginCombo;
+    KComboBox *mPluginCombo;
     KService::List mPluginList;
     KConfigSkeleton::ItemString *mItem;
 };
