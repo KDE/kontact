a649cddd59318f55c1fee5dd5fc330146f248f4b
Allen Winter <winter@kde.org>
2008-04-29 16:54:54 +0000

    Port Qt3 and K3 listwidget stuff to Qt4 QTreeWidget stuff
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=802501

--- plugins/summary/kcmkontactsummary.cpp
+++ plugins/summary/kcmkontactsummary.cpp
@@ -1,26 +1,30 @@
 /*
-    This file is part of KDE Kontact.
+  This file is part of KDE Kontact.
 
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2008 Allen Winter <winter@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
+
+#include "kcmkontactsummary.h"
+#include "plugin.h"
 
 #include <kaboutdata.h>
 #include <kconfig.h>
@@ -28,37 +32,33 @@
 #include <kdialog.h>
 #include <kiconloader.h>
 #include <klocale.h>
-#include <plugin.h>
+#include <kdemacros.h>
 #include <kplugininfo.h>
 #include <kservicetypetrader.h>
 #include <kcomponentdata.h>
-#include <QLayout>
+
 #include <QLabel>
-#include <QPixmap>
-//Added by qt3to4:
 #include <QVBoxLayout>
-
-#include "kcmkontactsummary.h"
-
-#include <kdemacros.h>
+#include <QTreeWidgetItem>
 
 extern "C"
 {
   KDE_EXPORT KCModule *create_kontactsummary( QWidget *parent, const char * ) {
-    KComponentData inst("kcmkontactsummary");
-    return new KCMKontactSummary( inst,parent );
+    KComponentData inst( "kcmkontactsummary" );
+    return new KCMKontactSummary( inst, parent );
   }
 }
 
-class PluginItem : public Q3CheckListItem
+class PluginItem : public QTreeWidgetItem
 {
   public:
-    PluginItem( const KPluginInfo &info, K3ListView *parent )
-      : Q3CheckListItem( parent, QString(), Q3CheckListItem::CheckBox ),
-        mInfo( info )
+    PluginItem( const KPluginInfo &info, QTreeWidget *parent )
+      : QTreeWidgetItem( parent ), mInfo( info )
     {
-      QPixmap pm = KIconLoader::global()->loadIcon( mInfo.icon(), KIconLoader::Small );
-      setPixmap( 0, pm );
+      setIcon( 0, KIcon( mInfo.icon() ) );
+      setText( 0, mInfo.name() );
+      setToolTip( 0, mInfo.comment() );
+      setFlags( Qt::ItemIsEnabled | Qt::ItemIsUserCheckable );
     }
 
     KPluginInfo pluginInfo() const
@@ -68,12 +68,13 @@ class PluginItem : public Q3CheckListItem
 
     virtual QString text( int column ) const
     {
-      if ( column == 0 )
+      if ( column == 0 ) {
         return mInfo.name();
-      else if ( column == 1 )
+      } else if ( column == 1 ) {
         return mInfo.comment();
-      else
+      } else {
         return QString();
+      }
     }
 
   private:
@@ -81,11 +82,10 @@ class PluginItem : public Q3CheckListItem
 };
 
 PluginView::PluginView( QWidget *parent )
-  : K3ListView( parent )
+  : QTreeWidget( parent )
 {
-  addColumn( i18n( "Name" ) );
-  setAllColumnsShowFocus( true );
-  setFullWidth( true );
+  setColumnCount( 1 );
+  setHeaderLabel( i18nc( "@title:column plugin name", "Summary Plugin Name" ) );
 }
 
 PluginView::~PluginView()
@@ -95,11 +95,13 @@ PluginView::~PluginView()
 KCMKontactSummary::KCMKontactSummary( const KComponentData &inst, QWidget *parent )
   : KCModule( inst, parent )
 {
+  setButtons( NoAdditionalButton );
   QVBoxLayout *layout = new QVBoxLayout( this );
   layout->setSpacing( KDialog::spacingHint() );
   layout->setMargin( 0 );
 
-  QLabel *label = new QLabel( i18n( "Here you can select which summary plugins to have visible in your summary view." ), this );
+  QLabel *label =
+    new QLabel( i18n( "Select the plugin summaries to show on the summary page." ), this );
   layout->addWidget( label );
 
   mPluginView = new PluginView( this );
@@ -107,8 +109,6 @@ KCMKontactSummary::KCMKontactSummary( const KComponentData &inst, QWidget *paren
 
   layout->setStretchFactor( mPluginView, 1 );
 
-  connect( mPluginView, SIGNAL( clicked( Q3ListViewItem* ) ),
-           this, SLOT( itemClicked( Q3ListViewItem* ) ) );
   load();
 
   KAboutData *about = new KAboutData( I18N_NOOP( "kontactsummary" ), 0,
@@ -116,7 +116,7 @@ KCMKontactSummary::KCMKontactSummary( const KComponentData &inst, QWidget *paren
                                       0, KLocalizedString(), KAboutData::License_GPL,
                                       ki18n( "(c), 2004 Tobias Koenig" ) );
 
-  about->addAuthor( ki18n("Tobias Koenig"), KLocalizedString(), "tokoe@kde.org" );
+  about->addAuthor( ki18n( "Tobias Koenig" ), KLocalizedString(), "tokoe@kde.org" );
   setAboutData( about );
 }
 
@@ -141,56 +141,52 @@ void KCMKontactSummary::load()
     activeSummaries << "kontact_newstickerplugin";
     activeSummaries << "kontact_plannerplugin";
   } else {
-    activeSummaries = grp.readEntry( "ActiveSummaries" , QStringList() );
+    activeSummaries = grp.readEntry( "ActiveSummaries", QStringList() );
   }
 
   mPluginView->clear();
 
-  KPluginInfo::List pluginList = KPluginInfo::fromServices( offers, KConfigGroup( &config, "Plugins" ) );
+  KPluginInfo::List pluginList =
+    KPluginInfo::fromServices( offers, KConfigGroup( &config, "Plugins" ) );
   KPluginInfo::List::Iterator it;
   for ( it = pluginList.begin(); it != pluginList.end(); ++it ) {
     it->load();
 
-    if ( !it->isPluginEnabled() )
+    if ( !it->isPluginEnabled() ) {
       continue;
+    }
 
     QVariant var = it->property( "X-KDE-KontactPluginHasSummary" );
-    if ( !var.isValid() )
-      continue;
-
-    if ( var.toBool() == true ) {
+    if ( var.isValid() && var.toBool() == true ) {
       PluginItem *item = new PluginItem( *it, mPluginView );
 
-      if ( activeSummaries.contains( it->pluginName() )  )
-        item->setOn( true );
+      if ( activeSummaries.contains( it->pluginName() ) ) {
+        item->setCheckState( 0, Qt::Checked );
+      } else {
+        item->setCheckState( 0, Qt::Unchecked );
+      }
     }
   }
 }
 
 void KCMKontactSummary::save()
 {
+  kDebug();
   QStringList activeSummaries;
 
-  Q3ListViewItemIterator it( mPluginView, Q3ListViewItemIterator::Checked );
-  while ( it.current() ) {
-    PluginItem *item = static_cast<PluginItem*>( it.current() );
-    activeSummaries.append( item->pluginInfo().pluginName() );
+  QTreeWidgetItemIterator it( mPluginView );
+  while ( *it ) {
+    PluginItem *item = static_cast<PluginItem *>( *it );
+    if ( item->checkState( 0 ) == Qt::Checked ) {
+      activeSummaries.append( item->pluginInfo().pluginName() );
+    }
     ++it;
   }
 
   KConfig config( "kontact_summaryrc" );
   KConfigGroup grp( &config, QString() );
+  kDebug() << "saving activesummaries " << activeSummaries;
   grp.writeEntry( "ActiveSummaries", activeSummaries );
-}
-
-void KCMKontactSummary::defaults()
-{
-  emit changed( true );
-}
-
-void KCMKontactSummary::itemClicked( Q3ListViewItem* )
-{
-  emit changed( true );
 }
 
 #include "kcmkontactsummary.moc"
