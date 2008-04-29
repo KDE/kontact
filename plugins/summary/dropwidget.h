185aecc398046c6068c956a4949de0e324082785
Allen Winter <winter@kde.org>
2008-04-29 17:02:26 +0000

    include header cleanup, forwarding classes coding style
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=802504

--- plugins/summary/dropwidget.h
+++ plugins/summary/dropwidget.h
@@ -1,31 +1,31 @@
 /*
-   This file is part of KDE Kontact.
+  This file is part of KDE Kontact.
 
-   Copyright (C) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (C) 2004 Tobias Koenig <tokoe@kde.org>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #ifndef DROP_WIDGET_H
 #define DROP_WIDGET_H
 
 #include <QWidget>
-//Added by qt3to4:
-#include <QDragEnterEvent>
-#include <QDropEvent>
+
+class QDragEnterEvent;
+class QDropEvent;
 
 class DropWidget : public QWidget
 {
@@ -38,8 +38,8 @@ class DropWidget : public QWidget
     void summaryWidgetDropped( QWidget *target, QWidget *widget, int alignment );
 
   protected:
-    virtual void dragEnterEvent( QDragEnterEvent* );
-    virtual void dropEvent( QDropEvent* );
+    virtual void dragEnterEvent( QDragEnterEvent * );
+    virtual void dropEvent( QDropEvent * );
 };
 
 #endif
