96f210290fac7239bfad30d4074c85034dfbfce7
Allen Winter <winter@kde.org>
2008-05-01 13:30:22 +0000

    reorder header inclusion coding style
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=802983

--- plugins/korganizer/kcmapptsummary.cpp
+++ plugins/korganizer/kcmapptsummary.cpp
@@ -1,41 +1,34 @@
 /*
-    This file is part of Kontact.
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
-    Copyright (c) 2005-2006 Allen Winter <winter@kde.org>
+  This file is part of Kontact.
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2005-2006,2008 Allen Winter <winter@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
-#include <QLabel>
-#include <QLayout>
-#include <QRadioButton>
-#include <QSpinBox>
+#include "kcmapptsummary.h"
 
 #include <kaboutdata.h>
 #include <kacceleratormanager.h>
 #include <kconfig.h>
-#include <kdebug.h>
-#include <klocale.h>
 #include <kcomponentdata.h>
-
-#include "kcmapptsummary.h"
-
+#include <klocale.h>
 #include <kdemacros.h>
 
 extern "C"
@@ -69,7 +62,6 @@ KCMApptSummary::~KCMApptSummary()
 {
 }
 
-
 void KCMApptSummary::modified()
 {
   emit changed( true );
@@ -86,11 +78,11 @@ void KCMApptSummary::load()
   KConfigGroup grp( &config, "Days" );
 
   int days = grp.readEntry( "DaysToShow", 7 );
-  if ( days == 1 )
+  if ( days == 1 ) {
     mDateTodayButton->setChecked( true );
-  else if ( days == 31 )
+  } else if ( days == 31 ) {
     mDateMonthButton->setChecked( true );
-  else {
+  } else {
     mDateRangeButton->setChecked( true );
     mCustomDays->setValue( days );
     mCustomDays->setEnabled( true );
@@ -128,7 +120,7 @@ void KCMApptSummary::defaults()
   emit changed( true );
 }
 
-const KAboutData* KCMApptSummary::aboutData() const
+const KAboutData *KCMApptSummary::aboutData() const
 {
   KAboutData *about = new KAboutData(
     I18N_NOOP( "kcmapptsummary" ), 0,
@@ -136,8 +128,8 @@ const KAboutData* KCMApptSummary::aboutData() const
     0, KLocalizedString(), KAboutData::License_GPL,
     ki18n( "(c) 2003 - 2004 Tobias Koenig" ) );
 
-  about->addAuthor( ki18n("Tobias Koenig"), KLocalizedString(), "tokoe@kde.org" );
-  about->addAuthor( ki18n("Allen Winter"), KLocalizedString(), "winter@kde.org" );
+  about->addAuthor( ki18n( "Tobias Koenig" ), KLocalizedString(), "tokoe@kde.org" );
+  about->addAuthor( ki18n( "Allen Winter" ), KLocalizedString(), "winter@kde.org" );
 
   return about;
 }
--- plugins/korganizer/kcmapptsummary.h
+++ plugins/korganizer/kcmapptsummary.h
@@ -1,33 +1,32 @@
 /*
-    This file is part of Kontact.
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
-    Copyright (c) 2005-2006 Allen Winter <winter@kde.org>
+  This file is part of Kontact.
+  Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2005-2006,2008 Allen Winter <winter@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #ifndef KCMAPPTSUMMARY_H
 #define KCMAPPTSUMMARY_H
 
-#include <kcmodule.h>
 #include "ui_apptsummaryconfig_base.h"
-
+#include <kcmodule.h>
 
 class KCMApptSummary : public KCModule, public Ui::ApptSummaryConfig_Base
 {
@@ -40,7 +39,7 @@ class KCMApptSummary : public KCModule, public Ui::ApptSummaryConfig_Base
     virtual void load();
     virtual void save();
     virtual void defaults();
-    virtual const KAboutData* aboutData() const;
+    virtual const KAboutData *aboutData() const;
 
   private slots:
     void modified();
