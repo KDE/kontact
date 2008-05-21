2dd100e71eb02a5c70fe1522cccb228857aa444c
Marc Mutz <mutz@kde.org>
2008-05-21 17:11:35 +0000

    Merged revisions 810762-810804 via svnmerge from svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r810762 | winterz | 2008-05-21 17:07:25 +0200 (Wed, 21 May 2008) | 3 lines
      
      clean up header inclusion
      coding style
    ........
      r810763 | winterz | 2008-05-21 17:08:28 +0200 (Wed, 21 May 2008) | 2 lines
      
      patch from FiNeX that fixes new filters clearing the list of existing filters.
    ........
      r810789 | scripty | 2008-05-21 17:53:03 +0200 (Wed, 21 May 2008) | 1 line
      
      SVN_SILENT made messages (.desktop file)
    ........
      r810804 | vkrause | 2008-05-21 18:18:05 +0200 (Wed, 21 May 2008) | 8 lines
      
      Fix crash during Kolab task folder refresh:
      - Don't emit dataChanged() during the reload, we might still have
        nodes that contain dangling pointers.
      - Keep a copy of the todo uid in the node since we need that in the node
        dtor and the todo pointer can be invalid at this point.
      
      Kolab issue 2697
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=810826

--- plugins/kjots/kjots_plugin.h
+++ plugins/kjots/kjots_plugin.h
@@ -1,25 +1,25 @@
 /*
-    This file is part of KJots.
+  This file is part of KJots.
 
-    Copyright (c) 2008 Stephen Kelly <steveire@gmail.com>
+  Copyright (c) 2008 Stephen Kelly <steveire@gmail.com>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #ifndef KJOTS_PLUGIN_H
@@ -27,11 +27,10 @@
 
 #include "kjotscomponentinterface.h"
 
-#include <klocale.h>
-#include <kparts/part.h>
-
 #include <kontactinterfaces/plugin.h>
 #include <kontactinterfaces/uniqueapphandler.h>
+
+#include <kparts/part.h>
 
 class KJotsUniqueAppHandler : public Kontact::UniqueAppHandler
 {
@@ -45,26 +44,22 @@ class KJotsPlugin : public Kontact::Plugin
   Q_OBJECT
 
   public:
-    KJotsPlugin( Kontact::Core *core, const QVariantList& );
+    KJotsPlugin( Kontact::Core *core, const QVariantList & );
     ~KJotsPlugin();
-    int weight() const { return 700; }
+    int weight() const { return 550; }
     virtual QStringList invisibleToolbarActions() const;
     virtual bool isRunningStandalone();
 
-    OrgKdeKJotsComponentInterface* interface();
-
+    OrgKdeKJotsComponentInterface *interface();
 
   private slots:
     void newPage();
     void newBook();
 
-
   protected:
-    KParts::ReadOnlyPart* createPart();
+    KParts::ReadOnlyPart *createPart();
     Kontact::UniqueAppWatcher *mUniqueAppWatcher;
     OrgKdeKJotsComponentInterface *m_interface;
-
-
 };
 
 #endif
