3a3120aa76b69239d6f2119a1e3a9133667b9f94
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-07-23 07:19:08 +0000

    SVN_SILENT Merged revisions 788634 via svnmerge from svn+ssh://pradeepto@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r788634 | ahartmetz | 2008-03-22 06:10:19 +0530 (Sat, 22 Mar 2008) | 1 line
      
      Update the copyright years
    ........
    
    svn path=/branches/KDE/3.5/kdepim/; revision=836855

--- src/main.cpp
+++ src/main.cpp
@@ -123,7 +123,7 @@ int KontactApp::newInstance()
 int main( int argc, char **argv )
 {
   KAboutData about( "kontact", I18N_NOOP( "Kontact" ), version, description,
-                    KAboutData::License_GPL, I18N_NOOP("(C) 2001-2004 The Kontact developers"), 0, "http://kontact.org" );
+                    KAboutData::License_GPL, I18N_NOOP("(C) 2001-2008 The Kontact developers"), 0, "http://kontact.org" );
   about.addAuthor( "Daniel Molkentin", 0, "molkentin@kde.org" );
   about.addAuthor( "Don Sanders", 0, "sanders@kde.org" );
   about.addAuthor( "Cornelius Schumacher", 0, "schumacher@kde.org" );
