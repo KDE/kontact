a96292e142af92fb9247b5a71be0a9480f53ac26
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-09-26 12:11:26 +0000

    update version numbers
    svn path=/branches/kdepim/enterprise/kdepim/; revision=865020

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.9 (enterprise35 0.20080919.862682)";
+static const char version[] = "1.2.9 (enterprise35 0.20080926.865016)";
 
 class KontactApp : public KUniqueApplication {
   public:
