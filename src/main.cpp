59ebb3c5c7a835f657cf7e3e253f25fa6f54e0b1
Pradeepto Bhattacharya <pradeepto@kde.org>
2009-01-30 18:54:04 +0000

    Update version numbers. SVN_SILENT
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=918783

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ using namespace std;
 static const char description[] =
   I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.3 (enterprise4 0.20090120.914273)";
+static const char version[] = "1.3 (enterprise4 0.20090130.918775)";
 
 
 
