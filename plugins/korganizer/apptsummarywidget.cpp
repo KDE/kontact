e88d9055bbde0b535d76fce86e85295e406b082e
Thomas McGuire <mcguire@kde.org>
2009-02-19 11:59:54 +0000

    Merged revisions 925749 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r925749 | smartins | 2009-02-14 03:32:15 +0100 (Sat, 14 Feb 2009) | 9 lines
      
      Resources were being loaded 3 times (apptsummary, todosummary and in korganizer's kpart).
      
      Now apptsummary and todosummary, instead of loading the resources, they just create korganizer's kpart (in case it doesn't exist 
      yet), which loads the resources only once.
      
      This also eliminates the "graphical bug" when switching to Summary, in which the agenda items disappeared right before the switch 
      (because the second resource load closed the calendar).
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=928315

--- plugins/korganizer/apptsummarywidget.cpp
+++ plugins/korganizer/apptsummarywidget.cpp
@@ -62,12 +62,13 @@ ApptSummaryWidget::ApptSummaryWidget( KOrganizerPlugin *plugin, QWidget *parent 
   mLayout->setRowStretch( 6, 1 );
 
   mCalendar = KOrg::StdCalendar::self();
-  mCalendar->load();
+
+  //If the kpart isn't created yet, it's created now, and mCalendar is loaded
+  mPlugin->part();
 
   connect( mCalendar, SIGNAL(calendarChanged()), SLOT(updateView()) );
   connect( mPlugin->core(), SIGNAL(dayChanged(const QDate&)),
            SLOT(updateView()) );
-
   updateView();
 }
 
