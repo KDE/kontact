478a887616da171fe8041619505cd19a7051c4c1
Allen Winter <winter@kde.org>
2009-02-24 13:31:22 +0000

    version number increase
    svn path=/branches/KDE/4.2/kdepim/; revision=930875

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ using namespace std;
 static const char description[] =
   I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.4";
+static const char version[] = "1.4.1";
 
 
 
