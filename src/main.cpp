1aa80c374ec3bf741488064812edf71579f664bc
Thomas McGuire <mcguire@kde.org>
2009-03-20 10:10:08 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=941668

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.9 (enterprise35 0.20090313.938843)";
+static const char version[] = "1.2.9 (enterprise35 0.20090320.940905)";
 
 class KontactApp : public KUniqueApplication {
   public:
