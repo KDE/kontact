7ac48c8a048f5a88bdd9aa1e85794b838b5f3ccc
Sergio Luis Martins <iamsergio@gmail.com>
2009-04-04 19:42:55 +0000

    Backport r947994 by smartins from trunk to the 4.2 branch:
    Fix plugin loading and/or unloading segfault.
    
    svn path=/branches/KDE/4.2/kdepim/; revision=949274

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -579,7 +579,6 @@ void MainWindow::updateShortcuts()
 bool MainWindow::removePlugin( const KPluginInfo &info )
 {
   PluginList::Iterator end = mPlugins.end();
-  ActionPluginList::Iterator it2 = mActionPlugins.begin();
   for ( PluginList::Iterator it = mPlugins.begin(); it != end; ++it ) {
     Plugin *plugin = *it;
     if ( ( *it )->identifier() == info.pluginName() ) {
@@ -608,8 +607,10 @@ bool MainWindow::removePlugin( const KPluginInfo &info )
       plugin->deleteLater(); // removes the part automatically
       mPlugins.erase( it );
       if ( plugin->showInSideBar() ) {
-        delete *it2; // remove the KAction, so we free the shortcut for later us
-        mActionPlugins.erase( it2 );
+        QAction *q = mPluginAction[plugin]; // remove the KAction, so we free the shortcut for later us
+        mActionPlugins.remove( q );
+        mPluginAction.remove(plugin);
+        delete q;
       }
 
       if ( mCurrentPlugin == 0 ) {
@@ -624,9 +625,6 @@ bool MainWindow::removePlugin( const KPluginInfo &info )
       return true;
     }
 
-    if ( plugin->showInSideBar() ) {
-      it2++;
-    }
   }
 
   return false;
