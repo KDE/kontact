8f811b394de4925bd5b2c934c7f908ca7ec8e2f4
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:46:40 +0000

    Updated Messages.sh files to pick up strings from .rc files.
    svn path=/trunk/KDE/kdepim/; revision=951700

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC src/*.kcfg >> rc.cpp || exit 11
-$EXTRACTRC $(find . -name "*.ui") >> rc.cpp || exit 12
+$EXTRACTRC $(find . -name "*.ui" -o -name "*.rc") >> rc.cpp || exit 12
 $XGETTEXT rc.cpp src/*.cpp plugins/*/*.cpp plugins/*/*.h -o $podir/kontact.pot
