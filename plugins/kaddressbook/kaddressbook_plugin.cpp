ca01b811aacab345101206779cbb03c868441ee0
Tobias Koenig <tokoe@kde.org>
2009-08-05 15:53:21 +0000

    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=1007358

---
+++ plugins/kaddressbook/kaddressbook_plugin.cpp
@@ -0,0 +1,135 @@
+/*
+    This file is part of KAddressBook Kontact Plugin.
+
+    Copyright (c) 2009 Laurent Montel <montel@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include "kaddressbook_plugin.h"
+#include "mainwidgetinterface.h"
+
+#include <kontactinterfaces/core.h>
+
+#include <kactioncollection.h>
+#include <kaction.h>
+#include <kcmdlineargs.h>
+#include <kdebug.h>
+#include <kgenericfactory.h>
+#include <kicon.h>
+#include <kiconloader.h>
+#include <kparts/componentfactory.h>
+
+
+EXPORT_KONTACT_PLUGIN( KAddressBookPlugin, kcontactmanager )
+
+KAddressBookPlugin::KAddressBookPlugin( Kontact::Core *core, const QVariantList & )
+  : Kontact::Plugin( core, core, "kcontactmanager" ), m_interface( 0 )
+{
+  setComponentData( KontactPluginFactory::componentData() );
+
+  KAction *action  = new KAction( KIcon( "contact-new" ),
+                                  i18n( "New Contact..." ), this );
+  actionCollection()->addAction( "new_contact", action );
+  connect( action, SIGNAL(triggered(bool)), SLOT(slotNewContact()) );
+  action->setShortcut( QKeySequence( Qt::CTRL + Qt::SHIFT + Qt::Key_C ) );
+  insertNewAction( action );
+
+  KAction *syncAction = new KAction( KIcon( "view-refresh" ),
+                                     i18n( "Synchronize Contacts" ), this );
+  actionCollection()->addAction( "kaddressbook_sync", syncAction );
+  connect( syncAction, SIGNAL(triggered(bool)), SLOT(slotSyncContacts()) );
+  insertSyncAction( syncAction );
+
+
+  mUniqueAppWatcher = new Kontact::UniqueAppWatcher(
+    new Kontact::UniqueAppHandlerFactory<KAddressBookUniqueAppHandler>(), this );
+
+}
+
+KAddressBookPlugin::~KAddressBookPlugin()
+{
+}
+
+void KAddressBookPlugin::slotNewContact()
+{
+  interface()->newContact();
+}
+
+QString KAddressBookPlugin::tipFile() const
+{
+  // TODO: tips file
+  //QString file = KStandardDirs::locate("data", "kcontactmanager/tips");
+  QString file;
+  return file;
+}
+
+KParts::ReadOnlyPart *KAddressBookPlugin::createPart()
+{
+  KParts::ReadOnlyPart *part = loadPart();
+  if ( !part ) {
+    return 0;
+  }
+  // Create the stub that allows us to talk to the part
+  m_interface = new OrgKdeKAddressBookMainWidgetInterface(
+    "org.kde.kaddressbook", "/KAddressBook", QDBusConnection::sessionBus() );
+//     org.kde.KContactmanager.MainWidget                                      /KAddressBook
+  return part;
+}
+
+OrgKdeKAddressBookMainWidgetInterface *KAddressBookPlugin::interface()
+{
+  if ( !m_interface ) {
+    part();
+  }
+  Q_ASSERT( m_interface );
+  return m_interface;
+}
+
+
+bool KAddressBookPlugin::isRunningStandalone()
+{
+  return mUniqueAppWatcher->isRunningStandalone();
+}
+
+
+QStringList KAddressBookPlugin::invisibleToolbarActions() const
+{
+  return QStringList( "file_new_contact" );
+}
+
+void KAddressBookPlugin::slotSyncContacts()
+{
+  QDBusMessage message =
+      QDBusMessage::createMethodCall( "org.kde.kmail", "/Groupware",
+                                      "org.kde.kmail.groupware",
+                                      "triggerSync" );
+  message << QString( "Contact" );
+  QDBusConnection::sessionBus().send( message );
+}
+
+void KAddressBookUniqueAppHandler::loadCommandLineOptions()
+{
+  KCmdLineArgs::addCmdLineOptions( KCmdLineOptions() );
+}
+
+int KAddressBookUniqueAppHandler::newInstance()
+{
+    kDebug() ;
+    // Ensure part is loaded
+    (void)plugin()->part();
+    return Kontact::UniqueAppHandler::newInstance();
+}
+
---
+++ plugins/kaddressbook/kaddressbook_plugin.h
@@ -0,0 +1,69 @@
+/*
+    This file is part of KAddressBook Kontact Plugin.
+
+    Copyright (c) 2009 Laurent Montel <montel@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KADDRESSBOOK_PLUGIN_H
+#define KADDRESSBOOK_PLUGIN_H
+
+
+#include <kontactinterfaces/plugin.h>
+#include <kontactinterfaces/uniqueapphandler.h>
+
+#include <kparts/part.h>
+
+class OrgKdeKAddressBookMainWidgetInterface;
+
+class KAddressBookUniqueAppHandler : public Kontact::UniqueAppHandler
+{
+  public:
+    KAddressBookUniqueAppHandler( Kontact::Plugin *plugin ) : Kontact::UniqueAppHandler( plugin ) {}
+    virtual void loadCommandLineOptions();
+    virtual int newInstance();
+};
+
+class KAddressBookPlugin : public Kontact::Plugin
+{
+  Q_OBJECT
+
+  public:
+    KAddressBookPlugin( Kontact::Core *core, const QVariantList & );
+    ~KAddressBookPlugin();
+
+    virtual QString tipFile() const;
+    virtual bool isRunningStandalone();
+    int weight() const { return 300; }
+
+    OrgKdeKAddressBookMainWidgetInterface *interface();
+    virtual QStringList invisibleToolbarActions() const;
+
+  protected:
+    KParts::ReadOnlyPart *createPart();
+
+  private slots:
+    void slotNewContact();
+    void slotSyncContacts();
+
+  private:
+    OrgKdeKAddressBookMainWidgetInterface *m_interface;
+    Kontact::UniqueAppWatcher *mUniqueAppWatcher;
+
+};
+
+#endif
+
