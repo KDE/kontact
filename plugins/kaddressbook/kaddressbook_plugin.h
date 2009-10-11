627dfb812fba6dd49ef775edb0931d66082fcb67
Allen Winter <winter@kde.org>
2009-10-11 23:08:33 +0000

    CamelCase headers remove unused headers MERGE: none
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=1034095

--- plugins/kaddressbook/kaddressbook_plugin.h
+++ plugins/kaddressbook/kaddressbook_plugin.h
@@ -1,30 +1,31 @@
 /*
-    This file is part of KAddressBook Kontact Plugin.
+  This file is part of KAddressBook Kontact Plugin.
 
-    Copyright (c) 2009 Laurent Montel <montel@kde.org>
+  Copyright (c) 2009 Laurent Montel <montel@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #ifndef KADDRESSBOOK_PLUGIN_H
 #define KADDRESSBOOK_PLUGIN_H
 
-#include <kontactinterface/plugin.h>
-#include <kontactinterface/uniqueapphandler.h>
+#include <KontactInterface/UniqueAppHandler>
 
-#include <kparts/part.h>
+namespace KontactInterface {
+  class Plugin;
+}
 
 class KAddressBookUniqueAppHandler : public KontactInterface::UniqueAppHandler
 {
