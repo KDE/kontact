b259477a631f4f492588bee65776efe0d74f6d23
Montel Laurent <montel@kde.org>
2015-12-29 14:53:23 +0100

    Remove them

--- Messages.sh
+++ Messages.sh
@@ -3,4 +3,4 @@
 $EXTRACTRC src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC $(find . -name "*.ui" -o -name "*.rc") >> rc.cpp || exit 12
 $XGETTEXT rc.cpp src/*.cpp plugins/*/*.cpp plugins/*/*.h -o $podir/kontact.pot
-rm -f rc.cpp html.cpp ../grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
+rm -f rc.cpp src/html.cpp ../grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
