d8df55244912a45f394464fc56f0dd82c9790c5c
l10n daemon script <scripty@kde.org>
2016-01-02 12:00:53 +0000

    SVN_SILENT made messages (after extraction)

--- src/html.cpp
+++ src/html.cpp
@@ -34,3 +34,12 @@ i18n("Kontact handles your e-mail, address book, calendar, to-do list and more."
 // i18n: file: src/about/introduction_kontact.html
 i18n("Skip this introduction");
 
+// i18n: file: src/about/loading_kontact.html
+i18n("Loading Kontact ...");
+// i18n: file: src/about/introduction_kontact.html
+i18n("Welcome to Kontact %1");
+// i18n: file: src/about/introduction_kontact.html
+i18n("Kontact handles your e-mail, address book, calendar, to-do list and more.");
+// i18n: file: src/about/introduction_kontact.html
+i18n("Skip this introduction");
+
