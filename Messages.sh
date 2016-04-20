dff0ad9ca5eaed84805a313c3a418e26e5ee15c1
Burkhard Lück <lueck@hube-lueck.de>
2016-04-20 14:11:17 +0200

    fix message extraction

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,5 @@
 ../grantlee-extractor-pot-scripts/extract_strings_ki18n.py `find -name \*.html` >> src/html.cpp
 $EXTRACTRC src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC $(find . -name "*.ui" -o -name "*.rc") >> rc.cpp || exit 12
-$XGETTEXT rc.cpp src/*.cpp plugins/*/*.cpp plugins/*/*.h -o $podir/kontact.pot
+$XGETTEXT rc.cpp src/*.cpp -o $podir/kontact.pot
 rm -f rc.cpp src/html.cpp ../grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
