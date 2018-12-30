71e314bc2cf1c73a63ca2c66495e8b5d9795b22f
Laurent Montel <montel@kde.org>
2018-12-30 10:46:56 +0100

    Update copyright

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ int main(int argc, char **argv)
                      QLatin1String(version),
                      i18n("KDE personal information manager"),
                      KAboutLicense::GPL,
-                     i18n("Copyright © 2001–2018 Kontact authors"),
+                     i18n("Copyright © 2001–2019 Kontact authors"),
                      QString(),
                      QStringLiteral("https://userbase.kde.org/Kontact"));
 
