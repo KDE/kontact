KDE/kontact

commit 684e1eab918597e33bce46071305fb888f280a8a

Start KOrgac via DBus activation on launch
Summary:
KOrgac is autostarted on session start, but just to be sure, we also try to launch it
on Kontact (and KOrganizer) start via DBus invocation.

Reviewers: #kde_pim, vkrause

Reviewed By: #kde_pim, vkrause

Subscribers: kde-pim

Tags: #kde_pim

Differential Revision: https://phabricator.kde.org/D29505

src/main.cpp
@@ -27,8 +27,6 @@
 #include "prefs.h"
 using namespace Kontact;
 
-#include <KdepimDBusInterfaces/ReminderClient>
-
 #include <KontactInterface/Plugin>
 #include <KontactInterface/UniqueAppHandler>
 #include <KontactInterface/PimUniqueApplication>
@@ -41,6 +39,8 @@ using namespace Kontact;
 #include <KCrash>
 
 #include <QCommandLineParser>
+#include <QDBusConnection>
+#include <QDBusConnectionInterface>
 
 #include <iostream>
 #include <kdelibs4configmigrator.h>
@@ -150,7 +150,7 @@ int KontactApp::activate(const QStringList &args, const QString &workingDir)
         mMainWindow->selectPlugin(moduleName);
     }
 
-    KPIM::ReminderClient::startDaemon();
+    QDBusConnection::sessionBus().interface()->startService(QStringLiteral("org.kde.kontact"));
 
     // Handle startup notification and window activation
     // (The first time it will do nothing except note that it was called)

CMakeLists.txt
@@ -64,7 +64,6 @@ find_package(KF5Akonadi ${AKONADI_VERSION} CONFIG REQUIRED)
 find_package(KF5KontactInterface ${KONTACTINTERFACE_LIB_VERSION} CONFIG REQUIRED)
 find_package(KF5Libkdepim ${LIBKDEPIM_LIB_VERSION_LIB} CONFIG REQUIRED)
 find_package(KF5GrantleeTheme ${LIBGRANTLEETHEME_LIB_VERSION_LIB} CONFIG REQUIRED)
-find_package(KF5KdepimDBusInterfaces ${KDEPIM_APPS_LIB_VERSION_LIB} CONFIG REQUIRED)
 
 
 include_directories(${kontact_SOURCE_DIR} ${kontact_BINARY_DIR})

src/CMakeLists.txt
@@ -63,7 +63,6 @@ set_target_properties(kontact_bin PROPERTIES OUTPUT_NAME kontact)
 
 target_link_libraries(kontact_bin
     KF5::Service
-    KF5::KdepimDBusInterfaces
     kontactprivate
     KF5::KontactInterface
     KF5::Crash

commit 2dc2a037b12cc8e0ca0c708767fe3440bcf7f92e

Fix starting KOrgac on Kontact start
Fixed a regression (due to a typo) introduced in 684e1eab9

CCMAIL: winter@kde.org

src/main.cpp
@@ -150,7 +150,8 @@ int KontactApp::activate(const QStringList &args, const QString &workingDir)
         mMainWindow->selectPlugin(moduleName);
     }
 
-    QDBusConnection::sessionBus().interface()->startService(QStringLiteral("org.kde.kontact"));
+    // Start KOrgac in case it's wasn't started on session start.
+    QDBusConnection::sessionBus().interface()->startService(QStringLiteral("org.kde.korgac"));
 
     // Handle startup notification and window activation
     // (The first time it will do nothing except note that it was called)