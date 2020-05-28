1ebcda0aafa28c0a06bf241286ce58fb18662600
Laurent Montel <montel@kde.org>
2020-05-28 20:38:29 +0200

    Fix Bug 422193 - start page don't display properly with qt 5.15
    CCBUG: 422193

--- src/webengine/introductionwebenginepage.cpp
+++ src/webengine/introductionwebenginepage.cpp
@@ -33,7 +33,7 @@ IntroductionWebEnginePage::IntroductionWebEnginePage(QObject *parent)
     settings()->setAttribute(QWebEngineSettings::XSSAuditingEnabled, false);
     settings()->setAttribute(QWebEngineSettings::ErrorPageEnabled, false);
     settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, false);
-    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, false);
+    //settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, false);
 
     settings()->setAttribute(QWebEngineSettings::ScreenCaptureEnabled, false);
     settings()->setAttribute(QWebEngineSettings::WebGLEnabled, false);
