f305fc91a63b14bf67fc6e635f1cb0fe9edab438
Laurent Montel <montel@kde.org>
2021-12-23 08:05:29 +0100

    GIT_SILENT: it's enabled by default too in qt6

--- src/main.cpp
+++ src/main.cpp
@@ -145,9 +145,9 @@ int main(int argc, char **argv)
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 #endif
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
     KontactApp app(argc, &argv);
 
     KCrash::initialize();
