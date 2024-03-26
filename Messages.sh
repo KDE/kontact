5fac22de3e90b9398df48a4edf4f6534eb7c7669
Laurent Montel <montel@kde.org>
2024-03-26 07:56:03 +0100

    We don't have ui file here

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find -name \*.html` >> rc.cpp
 $EXTRACTRC src/*.kcfg >> rc.cpp || exit 11
-$EXTRACTRC $(find . -name "*.ui" -o -name "*.rc") >> rc.cpp || exit 12
+$EXTRACTRC $(find . -name "*.rc") >> rc.cpp || exit 12
 $XGETTEXT rc.cpp src/*.cpp -o $podir/kontact.pot
 rm -f rc.cpp ./grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
