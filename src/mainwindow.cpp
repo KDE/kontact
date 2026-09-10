17fa8f872905b0a6a92f905265f5d3fd8efd61a3
Laurent Montel <montel@kde.org>
2026-09-10 20:12:39 +0200

    Make sure that cornerWidget is hidden

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -391,6 +391,8 @@ void MainWindow::slotFullScreen(bool t)
     } else {
         QWidget *w = mb->cornerWidget(Qt::TopRightCorner);
         if (w) {
+            mb->setCornerWidget(nullptr, Qt::TopRightCorner);
+            w->hide();
             w->deleteLater();
         }
     }
